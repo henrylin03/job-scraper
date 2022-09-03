5caca96fddf4d0220ca93d15592de5d79194adda
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-09-03 15:44:56 +1000

    add for loop to look through first 22 jobs (typically on page 1) and click

--- jobs-scraper.py
+++ jobs-scraper.py
@@ -13,10 +13,16 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 
 def extract_job_info():
-    search_result_jobs = driver.find_element(By.ID, "app")
-    # can I use regex on `class`, or just iterate through the index of Xpath for each job posting?
-
-    print(search_result_jobs.text)
+    for i in range(1, 23):
+        driver.execute_script(
+            "arguments[0].click();",
+            driver.find_element(
+                By.XPATH,
+                f'//*[@id="app"]/div/div[4]/div/section/div[2]/div/div[2]/div[1]/div/div/div[2]/div[{i}]/div/article/div[2]/a',
+            ),
+        )
+    # //*[@id="app"]/div/div[4]/div/section/div[2]/div/div[2]/div[1]/div/div/div[2]/div[1]/div/article/div[2]/a
+    # //*[@id="app"]/div/div[4]/div/section/div[2]/div/div[2]/div[1]/div/div/div[2]/div[22]/div/article/div[2]/a
 
 
 def main():

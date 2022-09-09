5d0e82f88d24f162c55a021a6f0882d58eef813a
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-09-09 22:04:07 +1000

    find job title from jobs using xpaths as classes are compound and Selenium does not support compound classes (ie where there are spaces in the class names)

--- jobs-scraper.py
+++ jobs-scraper.py
@@ -21,13 +21,18 @@ def search(what, where):
 
 
 def extract_job_info():
-    #! should go for elements on page that has all information that we want to scrape - potentially a class
-    # search_results = driver.find_elements(By.XPATH, '//*[@id="job_45fce791cd94c471"]')
-    search_results = driver.find_elements(By.XPATH, '//*[starts-with(@id, "job_")]')
-    for r in search_results:
-        print(r.text)
+    jobs = driver.find_elements(
+        By.XPATH, '//div[@class="slider_container css-g7s71f eu4oa1w0"]'
+    )
+    for job in jobs:
+        job_title = job.find_element(
+            By.XPATH, ".//*[starts-with(@id, 'jobTitle')]"
+        ).text
+        print(job_title)
 
-    # '//*[@id="mosaic-provider-jobcards"]/ul/li[7]/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[2]/span[1]/a'
+
+# '//*[@id="mosaic-provider-jobcards"]/ul/li[11]/div'
+# '//*[@id="mosaic-provider-jobcards"]/ul/li[13]/div'
 
 
 def main():

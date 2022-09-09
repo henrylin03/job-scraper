d83b9628052750fd503f0b6fa21882c518b40af1
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-09-09 20:24:27 +1000

    find xpath starting with 'job_' for all job names on page

--- jobs-scraper.py
+++ jobs-scraper.py
@@ -20,10 +20,21 @@ def search(what, where):
     ).send_keys_to_element(where_searchbox, where).send_keys(Keys.ENTER).perform()
 
 
+def extract_job_info():
+    #! should go for elements on page that has all information that we want to scrape - potentially a class
+    # search_results = driver.find_elements(By.XPATH, '//*[@id="job_45fce791cd94c471"]')
+    search_results = driver.find_elements(By.XPATH, '//*[starts-with(@id, "job_")]')
+    for r in search_results:
+        print(r.text)
+
+    # '//*[@id="mosaic-provider-jobcards"]/ul/li[7]/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[2]/span[1]/a'
+
+
 def main():
     # open given url (later, can have sign-in options, search-bar etc)
     driver.get(url_indeed)
     search("business analyst", "sydney")
+    extract_job_info()
 
 
 if __name__ == "__main__":

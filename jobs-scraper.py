0cfd7f45c94bcbe423e89fed3d0733ee7d9b1e50
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-09-12 17:17:50 +1000

    add windows size option and setup_chrome_driver function; expand ticket by clicking on the job title of the results on left pane

--- jobs-scraper.py
+++ jobs-scraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
@@ -9,7 +10,17 @@ import pandas as pd
 
 
 URL_INDEED = "https://au.indeed.com/"
-DRIVER = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+def setup_chrome_driver():
+    options = Options()
+    options.add_argument("window-size=1920,1080")
+    return webdriver.Chrome(
+        options=options, service=Service(ChromeDriverManager().install())
+    )
+
+
+DRIVER = setup_chrome_driver()
 
 
 def search(what, where):
@@ -21,32 +32,38 @@ def search(what, where):
 
 
 def extract_job_info():
-    # jobs = DRIVER.find_elements(
-    #     By.XPATH, '//div[@class="slider_container css-g7s71f eu4oa1w0"]'
-    # )
-    jobs = DRIVER.find_elements(By.CLASS_NAME, "job_seen_beacon")
+    jobs = DRIVER.find_elements(
+        By.XPATH, '//*[@class="slider_container css-g7s71f eu4oa1w0"]'
+    )
     for j in jobs:
-        DRIVER.execute_script("arguments[0].click();", j)
-        job_title = j.find_element(By.XPATH, ".//*[starts-with(@id, 'jobTitle')]").text
-        job_poster = j.find_element(By.CLASS_NAME, "companyName").text
+        title_link = j.find_element(By.XPATH, ".//*[starts-with(@id, 'jobTitle')]")
+        DRIVER.execute_script("arguments[0].click();", title_link)
+        title = title_link.text
+        poster = j.find_element(By.CLASS_NAME, "companyName").text
         location = j.find_element(By.CLASS_NAME, "companyLocation").text
         try:
-            # salary_estimate = j.find_element(By.CLASS_NAME, "attribute_snippet").text
             salary_estimate = j.find_element(
                 By.XPATH, './/div[@class="metadata salary-snippet-container"]'
             ).text
         except NoSuchElementException:
             salary_estimate = ""
-        print(job_title, job_poster, location, salary_estimate, sep=" - ")
+        # jobs_expanded = DRIVER.find_element(By.CLASS_NAME, "jobsearch-RightPane")
+        # job_description = jobs_expanded.find_element(
+        #     By.CLASS_NAME, "jobsearch-jobDescriptionText"
+        # ).text
+        print(title, poster, location, salary_estimate, sep=" - ")
 
 
-'//*[@id="mosaic-provider-jobcards"]/ul/li[8]/div/div[1]/div/div[1]/div/table[1]/tbody/tr/td/div[3]/div[1]'
+# right pane
+'//*[@id="jobsearch-JapanPage"]/div/div/div[5]/div[2]'
+'//*[@id="jobsearch-JapanPage"]/div/div/div[5]/div[2]'
 
 
 def main():
-    # open given url (later, can have sign-in options, search-bar etc)
     DRIVER.get(URL_INDEED)
-    search("business analyst remote", "australia")
+    search(
+        "business analyst remote", "australia"
+    )  # have conditional that if the word "remote" is in the name to also filter on "remote" to not only rely on the keywords to search
     extract_job_info()
 
 

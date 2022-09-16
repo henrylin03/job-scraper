e6b4429c6c66f4b721797ce29689283ef5f85387
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2022-09-16 18:06:43 +1000

    add popover closer

--- jobs-scraper.py
+++ jobs-scraper.py
@@ -1,10 +1,14 @@
+from ast import Pass
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
-from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 import pandas as pd
 import openpyxl
 
@@ -12,6 +16,7 @@ import openpyxl
 def setup_chrome_driver():
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    # options.add_argument("--start-maximized")
     return webdriver.Chrome(
         options=options, service=Service(ChromeDriverManager().install())
     )
@@ -27,6 +32,67 @@ def search(what, where):
         what_searchbox, what
     ).send_keys_to_element(where_searchbox, where).send_keys(Keys.ENTER).perform()
     return DRIVER.current_url
+
+
+def extract_job_info():
+    jobs_list = []
+    jobs = DRIVER.find_elements(
+        By.XPATH, '//*[@class="slider_container css-g7s71f eu4oa1w0"]'
+    )
+    try:
+        WebDriverWait(DRIVER, 5).until(
+            EC.element_to_be_clickable((By.XPATH, '//*[@id="popover-x"]/button'))
+        ).click()
+    except TimeoutException:
+        pass
+    for j in jobs:
+        title_link = j.find_element(By.XPATH, ".//*[starts-with(@id, 'jobTitle')]")
+        DRIVER.execute_script("arguments[0].click();", title_link)
+        poster = j.find_element(By.CLASS_NAME, "companyName").text
+        location = j.find_element(By.CLASS_NAME, "companyLocation").text
+        try:
+            salary_estimate = j.find_element(
+                By.XPATH, './/div[@class="metadata salary-snippet-container"]'
+            ).text
+        except NoSuchElementException:
+            salary_estimate = ""
+        try:
+            jobs_expanded = DRIVER.find_element(
+                By.XPATH,
+                '//*[@id="viewJobSSRRoot"]',
+            )
+            ActionChains(DRIVER).move_to_element(jobs_expanded).perform()
+            description_element = jobs_expanded.find_element(
+                By.XPATH, './/*[@id="jobDescriptionText"]'
+            )
+
+        except NoSuchElementException:
+            try:
+                jobs_expanded = DRIVER.find_element(
+                    By.XPATH, '//*[@id="jobsearch-JapanPage"]/div/div/div[5]/div[2]'
+                )
+                ActionChains(DRIVER).move_to_element(jobs_expanded).perform()
+                description_element = jobs_expanded.find_element(
+                    By.ID, "jobDescriptionText"
+                )
+            except NoSuchElementException:
+                jobs_expanded = DRIVER.find_element(By.ID, "vjs-container")
+                ActionChains(DRIVER).move_to_element(jobs_expanded).perform()
+                description_element = DRIVER.find_element(
+                    By.XPATH, './/*[@class="jobsearch-JobComponent-embeddedBody"]'
+                )
+        ActionChains(DRIVER).move_to_element(description_element).perform()
+        description_element.text
+        jobs_dict = {
+            "title": title_link.text,
+            "poster": poster,
+            "location": location,
+            "description": description_element.text,
+            "estimated pay": salary_estimate,
+            "link": DRIVER.current_url,
+        }
+        jobs_list.append(jobs_dict)
+    return pd.DataFrame(jobs_list)
 
 
 def extract_pages(page1_url, page_count=3):
@@ -47,63 +113,10 @@ def extract_pages(page1_url, page_count=3):
     df_cleaned.to_excel("output.xlsx", index=False)
 
 
-def extract_job_info():
-    jobs_list = []
-    jobs = DRIVER.find_elements(
-        By.XPATH, '//*[@class="slider_container css-g7s71f eu4oa1w0"]'
-    )
-    for j in jobs:
-        title_link = j.find_element(By.XPATH, ".//*[starts-with(@id, 'jobTitle')]")
-        DRIVER.execute_script("arguments[0].click();", title_link)
-        poster = j.find_element(By.CLASS_NAME, "companyName").text
-        location = j.find_element(By.CLASS_NAME, "companyLocation").text
-        try:
-            salary_estimate = j.find_element(
-                By.XPATH, './/div[@class="metadata salary-snippet-container"]'
-            ).text
-        except NoSuchElementException:
-            salary_estimate = ""
-        try:
-            jobs_expanded = DRIVER.find_element(
-                By.XPATH, '//*[@id="viewJobSSRRoot"]/div[2]/div/div/div/div/div[1]/div'
-            )
-            ActionChains(DRIVER).move_to_element(jobs_expanded).perform()
-            job_description = jobs_expanded.find_element(
-                By.XPATH, './/*[@id="jobDescriptionText"]'
-            ).text
-        except NoSuchElementException:
-            try:
-                jobs_expanded = DRIVER.find_element(
-                    By.XPATH, '//*[@id="jobsearch-JapanPage"]/div/div/div[5]/div[2]'
-                )
-                ActionChains(DRIVER).move_to_element(jobs_expanded).perform()
-                job_description = jobs_expanded.find_element(
-                    By.ID, "jobDescriptionText"
-                ).text
-            except NoSuchElementException:
-                description_element = DRIVER.find_element(
-                    By.ID, "vjs-container"
-                ).find_element(
-                    By.XPATH, './/*[@class="jobsearch-JobComponent-embeddedBody"]'
-                )
-                ActionChains(DRIVER).move_to_element(description_element).perform()
-                job_description = description_element.text
-        jobs_dict = {
-            "title": title_link.text,
-            "poster": poster,
-            "location": location,
-            "description": job_description,
-            "estimated pay": salary_estimate,
-            "link": DRIVER.current_url,
-        }
-        jobs_list.append(jobs_dict)
-    return pd.DataFrame(jobs_list)
-
-
 def main():
     DRIVER.get("https://au.indeed.com/")
     search_url = search("senior business analyst remote", "australia")
-    extract_pages(search_url, 0.5)
+    extract_pages(search_url)
 
 
 if __name__ == "__main__":
